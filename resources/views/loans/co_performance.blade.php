1c562cac038854421ce2a9d83609d5e1c7e00967
MYSEY <52909452+MYSEY@users.noreply.github.com>
2026-02-27 11:32:30 +0700

    update

--- resources/views/loans/co_performance.blade.php
+++ resources/views/loans/co_performance.blade.php
@@ -107,7 +107,7 @@
                             Search
                             <span class="loading-icon" style="display: none"><i class="fa fa-spinner fa-spin"></i></span>
                         </button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary btn_excel mr-1" id="icon-search-download-reload">
+                        <button type="button" class="btn btn-sm btn-info waves-effect waves-themed btn_excel mr-1" id="icon-search-download-reload">
                             <span class="btn-text-excel"><i class="fal fa-arrow-circle-down"></i></span>
                             Excel
                             <span id="btn-text-loading-excel" style="display: none"><i class="fa fa-spinner fa-spin"></i></span>
