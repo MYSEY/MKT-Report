0564268d00160c782e20292fc3a474bd9a5f2826
MYSEY <52909452+MYSEY@users.noreply.github.com>
2026-04-08 09:31:33 +0700

    update download loan detail

--- app/Http/Controllers/Admins/LoandDetailListingController.php
+++ app/Http/Controllers/Admins/LoandDetailListingController.php
@@ -24,181 +24,12 @@ class LoandDetailListingController extends Controller
             return view('page.access_page');
         }
         if (request()->ajax()) {
-            $subQueryPD = DB::connection('pgsql')
-            ->table(DB::raw('(
-                SELECT DISTINCT ON ("ID")
-                    "ID",
-                    CAST("NumDayDue" AS INTEGER) AS "DueDay",
-                    "DueDate"
-                FROM "MKT_PD_DATE"
-                WHERE "OutIntAmountAS" > 0 OR "OutPriAmountAS" > 0
-                ORDER BY "ID", CAST("NumDayDue" AS INTEGER) DESC
-            ) as PD'));
-            
-            $subQueryACCENTR = DB::connection('pgsql')
-            ->table('MKT_ACC_ENTRY')
-            ->select(
-                'Account',
-                // 'Reference',
-                DB::raw('MAX("Reference") AS "Reference"'),
-                DB::raw('MAX("TransactionDate") AS "LastPaymentDate"'),
-            )
-            ->where('Amount', '>', 0)
-            ->groupBy('Account');
-            
-            $query = DB::connection('pgsql')
-                ->table('MKT_LOAN_CONTRACT as LC')
-                ->select([
-                    'LC.ID',
-                    'LC.ContractCustomerID',
-                    'LC.Branch',
-                    'LC.Account',
-                    'LC.Currency',
-                    'LC.Disbursed',
-                    'LC.LoanBalanceAS',
-                    'LC.OutstandingAmountAS',
-                    'LC.InterestRate',
-                    'LC.AIRAS',
-                    'LC.AIRCurrentAS',
-                    'LC.AccrIntPerDay',
-                    'LC.TotalInterest',
-                    'LC.ValueDate',
-                    'LC.MaturityDate',
-                    'LC.Term',
-                    'LC.DisbursedStat',
-                    'LC.AssetClass',
-                    'LC.MoreThanOneYear',
-                    'LC.CBCSubSection',
-                    'LC.LoanPurpose',
-                    'LC.ContractOfficerID',
-                    'LC.LoanType',
-                    'LC.RestructuredCycle',
-                    'LCol.Collateral as CollateralID',
-                    'LC.Amount',
-                    'LC.OutstandingAmount',
-                    'LC.EIRRate',
-                    'LC.AccrInterest',
-                    'LC.IntIncEarned',
-                    'LC.Sector as MACode',
-                    'LC.LoanProduct',
-                    'LC.Cycle',
-                    'LC.SubAmount',
-                    'LC.SubLoanPurpose',
-                    'LC.PartneredWith',
-                    'LC.RestructureType',
-                    'CUST.LastNameEn',
-                    'CUST.FirstNameEn',
-                    'CUST.Gender',
-                    'CUST.IDType',
-                    'CUST.IDNumber',
-                    'CUST.Mobile1',
-                    'CUST.Mobile2',
-                    'CUST.HouseNo',
-                    'CUST.CBCISSubSection as CBCISSubSectionCuSt',
-                    'CUST.Village as AddressCode',
-                    'CUST.Street',
-                    'VL.LocalDescription as Village',
-                    'CM.LocalDescription as Commune',
-                    'DS.LocalDescription as District',
-                    'PR.LocalDescription as Province',
-                    'Sct.Description as MADes',
-                    'LPr.Description as LoanProductDes',
-                    'PD.DueDay',
-                    'PD.DueDate as OverdueDate',
-                    'LCh1.Charge AS LoanCharge101',
-                    'LCh1.Charge AS LoanCharge',
-                    'LCh1.ChargeEarned',
-                    'LCh1.ChargeUnearned',
-                    'LCh2.Charge AS LoanCharge102',
-                    'LCh2.Charge as RegularCharge',
-                    'POS.Description as CustomerOccupation',
-                    'SD.RepMode as ScheduleType',
-
-                    // ✅ FINAL FIX (correlated subquery)
-                    // DB::raw('(
-                    //     SELECT MAX(AE."TransactionDate")
-                    //     FROM "MKT_ACC_ENTRY" AE
-                    //     WHERE AE."Account" = "LC"."Account"
-                    //     AND AE."Amount" > 0
-                    //     AND (
-                    //         AE."Reference" = "LC"."ID"
-                    //         OR AE."Reference" = \'PD\' || "LC"."ID"
-                    //     )
-                    // ) as "LastPaymentDate"')
-                    DB::raw('(
-                        SELECT MAX(AE."TransactionDate")
-                        FROM "MKT_ACC_ENTRY" AE
-                        WHERE AE."Account" = "LC"."Account"
-                        AND AE."Amount" > 0
-                        AND AE."Transaction" IN (\'6\',\'7\',\'10\',\'11\',\'13\',\'25\',\'27\',\'40\',\'52\',\'53\',\'54\')
-                        AND (
-                            AE."Reference" = "LC"."ID"
-                            OR AE."Reference" = \'PD\' || "LC"."ID"
-                        )
-                    ) as "LastPaymentDate"')
-                ])
-
-                // ✅ PD Subquery Join
-                ->leftJoinSub($subQueryPD, 'PD', function ($join) {
-                    $join->whereRaw('"PD"."ID" = \'PD\' || "LC"."ID"');
-                })
-
-                // Other joins
-                ->leftJoin('MKT_LOAN_CHARGE as LCh1', function($q){
-                    $q->on('LC.ID', '=', 'LCh1.ID')
-                    ->where('LCh1.ChargeKey', '=', 101);
-                })
-                ->leftJoin('MKT_LOAN_CHARGE as LCh2', function($q){
-                    $q->on('LC.ID', '=', 'LCh2.ID')
-                    ->where('LCh2.ChargeKey', '=', 102);
-                })
-                ->leftJoin('MKT_CUSTOMER as CUST', 'LC.ContractCustomerID', '=', 'CUST.ID')
-                ->leftJoin('MKT_SCHED_DEFINE as SD', 'LC.ID', '=', 'SD.ID')
-                ->leftJoin('MKT_POSITION as POS', 'POS.ID', '=', 'CUST.Position')
-                ->leftJoin('MKT_VILLAGE as VL', 'CUST.Village', '=', 'VL.ID')
-                ->leftJoin('MKT_COMMUNE as CM', 'CUST.Commune', '=', 'CM.ID')
-                ->leftJoin('MKT_DISTRICT as DS', 'CUST.District', '=', 'DS.ID')
-                ->leftJoin('MKT_PROVINCE as PR', 'CUST.Province', '=', 'PR.ID')
-                ->leftJoin('MKT_SECTOR as Sct', 'LC.Sector', '=', 'Sct.ID')
-                ->leftJoin('MKT_LOAN_COLLATERAL as LCol', 'LC.ID', '=', 'LCol.ID')
-                ->leftJoin('MKT_LOAN_PRODUCT as LPr', 'LC.LoanProduct', '=', 'LPr.ID');
-                // ->where('LC.OutstandingAmountAS', '>', 0)
-                // ->where('LC.Branch', '<>', '""');
-
-                $query->when($request->branch_id, fn($q,$branch_id) =>
-                    $q->where('LC.Branch', $branch_id)
-                );
-                $query->when($request->LCID, fn($q,$LCID) =>
-                    $q->where('LC.ID', 'ilike', "%{$LCID}%")
-                );
-                
-                $searchValue = request()->input('search.value');
-                if (!empty($searchValue)) {
-                    $query->where(function ($q) use ($searchValue) {
-                        $q->where('LC.Account', 'like', "%{$searchValue}%")
-                        ->orWhere('LC.ID', 'like', "%{$searchValue}%")
-                        ->orWhere('CUST.FirstNameEn', 'like', "%{$searchValue}%")
-                        ->orWhere('CUST.LastNameEn', 'like', "%{$searchValue}%")
-                        ->orWhere('LC.ContractCustomerID', 'like', "%{$searchValue}%")
-                        ->orWhere('LC.ContractOfficerID', 'like', "%{$searchValue}%")
-                        ->orWhere('PR.LocalDescription', 'like', "%{$searchValue}%")
-                        ->orWhere('DS.LocalDescription', 'like', "%{$searchValue}%")
-                        ->orWhere('CM.LocalDescription', 'like', "%{$searchValue}%")
-                        ->orWhere('VL.LocalDescription', 'like', "%{$searchValue}%")
-                        ->orWhere('Sct.Description', 'like', "%{$searchValue}%")
-                        ->orWhere('LPr.Description', 'like', "%{$searchValue}%");
-                    });
-                }
-
-
-            // Total rows (no search)
+            $query = self::getDatas($request);
             $recordsTotal = DB::connection('pgsql')
             ->table('MKT_LOAN_CONTRACT as LC')
             ->when($request->filled('branch_id'), function ($q) use ($request) {
                 $q->where('LC.Branch', $request->branch_id);
             })->count();
-
-            // Total rows (with search)
             $recordsFiltered = $query->count();
             // Pagination
             $start = intval(request()->input('start', 0));
@@ -218,13 +49,163 @@ class LoandDetailListingController extends Controller
     }
     public function download(Request $request){
         $data = DB::connection('pgsql')->table('MKT_DATES')->select('ID', 'SystemDate')->first();
-        // Convert to Carbon
         $date = Carbon::parse($data->SystemDate);
         $currentTime = now()->setTimezone('Asia/Phnom_Penh');
-        // Add current time
         $dateTime = $date->format('Y-m-d') . '-' . $currentTime->format('H-i');
-        // File name
         $fileName = "Loan Detail Listing {$dateTime}.xlsx";
-        return Excel::download(new ExportLoanDetailListing($request), $fileName);
+        $query = self::getDatas($request);
+        $data = $query->get();
+        return Excel::download(new ExportLoanDetailListing($data), $fileName);
+    }
+
+    public static function getDatas($request)
+    {
+        $subQueryPD = DB::connection('pgsql')
+        ->table(DB::raw('(
+            SELECT DISTINCT ON ("ID")
+                "ID",
+                CAST("NumDayDue" AS INTEGER) AS "DueDay",
+                "DueDate"
+            FROM "MKT_PD_DATE"
+            WHERE "OutIntAmountAS" > 0 OR "OutPriAmountAS" > 0
+            ORDER BY "ID", CAST("NumDayDue" AS INTEGER) DESC
+        ) as PD'));
+        
+        $subQueryACCENTR = DB::connection('pgsql')
+        ->table('MKT_ACC_ENTRY')
+        ->select(
+            'Account',
+            DB::raw('MAX("Reference") AS "Reference"'),
+            DB::raw('MAX("TransactionDate") AS "LastPaymentDate"'),
+        )
+        ->where('Amount', '>', 0)
+        ->groupBy('Account');
+        
+        $query = DB::connection('pgsql')
+            ->table('MKT_LOAN_CONTRACT as LC')
+            ->select([
+                'LC.ID',
+                'LC.ContractCustomerID',
+                'LC.Branch',
+                'LC.Account',
+                'LC.Currency',
+                'LC.Disbursed',
+                'LC.LoanBalanceAS',
+                'LC.OutstandingAmountAS',
+                'LC.InterestRate',
+                'LC.AIRAS',
+                'LC.AIRCurrentAS',
+                'LC.AccrIntPerDay',
+                'LC.TotalInterest',
+                'LC.ValueDate',
+                'LC.MaturityDate',
+                'LC.Term',
+                'LC.DisbursedStat',
+                'LC.AssetClass',
+                'LC.MoreThanOneYear',
+                'LC.CBCSubSection',
+                'LC.LoanPurpose',
+                'LC.ContractOfficerID',
+                'LC.LoanType',
+                'LC.RestructuredCycle',
+                'LCol.Collateral as CollateralID',
+                'LC.Amount',
+                'LC.OutstandingAmount',
+                'LC.EIRRate',
+                'LC.AccrInterest',
+                'LC.IntIncEarned',
+                'LC.Sector as MACode',
+                'LC.LoanProduct',
+                'LC.Cycle',
+                'LC.SubAmount',
+                'LC.SubLoanPurpose',
+                'LC.PartneredWith',
+                'LC.RestructureType',
+                'CUST.LastNameEn',
+                'CUST.FirstNameEn',
+                'CUST.Gender',
+                'CUST.IDType',
+                'CUST.IDNumber',
+                'CUST.Mobile1',
+                'CUST.Mobile2',
+                'CUST.HouseNo',
+                'CUST.CBCISSubSection as CBCISSubSectionCuSt',
+                'CUST.Village as AddressCode',
+                'CUST.Street',
+                'VL.LocalDescription as Village',
+                'CM.LocalDescription as Commune',
+                'DS.LocalDescription as District',
+                'PR.LocalDescription as Province',
+                'Sct.Description as MADes',
+                'LPr.Description as LoanProductDes',
+                'PD.DueDay',
+                'PD.DueDate as OverdueDate',
+                'LCh1.Charge AS LoanCharge101',
+                'LCh1.Charge AS LoanCharge',
+                'LCh1.ChargeEarned',
+                'LCh1.ChargeUnearned',
+                'LCh2.Charge AS LoanCharge102',
+                'LCh2.Charge as RegularCharge',
+                'POS.Description as CustomerOccupation',
+                'SD.RepMode as ScheduleType',
+                DB::raw('(
+                    SELECT MAX(AE."TransactionDate")
+                    FROM "MKT_ACC_ENTRY" AE
+                    WHERE AE."Account" = "LC"."Account"
+                    AND AE."Amount" > 0
+                    AND AE."Transaction" IN (\'6\',\'7\',\'10\',\'11\',\'13\',\'25\',\'27\',\'40\',\'52\',\'53\',\'54\')
+                    AND (
+                        AE."Reference" = "LC"."ID"
+                        OR AE."Reference" = \'PD\' || "LC"."ID"
+                    )
+                ) as "LastPaymentDate"')
+            ])
+            ->leftJoinSub($subQueryPD, 'PD', function ($join) {
+                $join->whereRaw('"PD"."ID" = \'PD\' || "LC"."ID"');
+            })
+            ->leftJoin('MKT_LOAN_CHARGE as LCh1', function($q){
+                $q->on('LC.ID', '=', 'LCh1.ID')
+                ->where('LCh1.ChargeKey', '=', 101);
+            })
+            ->leftJoin('MKT_LOAN_CHARGE as LCh2', function($q){
+                $q->on('LC.ID', '=', 'LCh2.ID')
+                ->where('LCh2.ChargeKey', '=', 102);
+            })
+            ->leftJoin('MKT_CUSTOMER as CUST', 'LC.ContractCustomerID', '=', 'CUST.ID')
+            ->leftJoin('MKT_SCHED_DEFINE as SD', 'LC.ID', '=', 'SD.ID')
+            ->leftJoin('MKT_POSITION as POS', 'POS.ID', '=', 'CUST.Position')
+            ->leftJoin('MKT_VILLAGE as VL', 'CUST.Village', '=', 'VL.ID')
+            ->leftJoin('MKT_COMMUNE as CM', 'CUST.Commune', '=', 'CM.ID')
+            ->leftJoin('MKT_DISTRICT as DS', 'CUST.District', '=', 'DS.ID')
+            ->leftJoin('MKT_PROVINCE as PR', 'CUST.Province', '=', 'PR.ID')
+            ->leftJoin('MKT_SECTOR as Sct', 'LC.Sector', '=', 'Sct.ID')
+            ->leftJoin('MKT_LOAN_COLLATERAL as LCol', 'LC.ID', '=', 'LCol.ID')
+            ->leftJoin('MKT_LOAN_PRODUCT as LPr', 'LC.LoanProduct', '=', 'LPr.ID');
+
+            $query->when($request->branch_id, fn($q,$branch_id) =>
+                $q->where('LC.Branch', $branch_id)
+            );
+            $query->when($request->LCID, fn($q,$LCID) =>
+                $q->where('LC.ID', 'ilike', "%{$LCID}%")
+            );
+            
+            $searchValue = request()->input('search.value');
+            if (!empty($searchValue)) {
+                $query->where(function ($q) use ($searchValue) {
+                    $q->where('LC.Account', 'like', "%{$searchValue}%")
+                    ->orWhere('LC.ID', 'like', "%{$searchValue}%")
+                    ->orWhere('CUST.FirstNameEn', 'like', "%{$searchValue}%")
+                    ->orWhere('CUST.LastNameEn', 'like', "%{$searchValue}%")
+                    ->orWhere('LC.ContractCustomerID', 'like', "%{$searchValue}%")
+                    ->orWhere('LC.ContractOfficerID', 'like', "%{$searchValue}%")
+                    ->orWhere('PR.LocalDescription', 'like', "%{$searchValue}%")
+                    ->orWhere('DS.LocalDescription', 'like', "%{$searchValue}%")
+                    ->orWhere('CM.LocalDescription', 'like', "%{$searchValue}%")
+                    ->orWhere('VL.LocalDescription', 'like', "%{$searchValue}%")
+                    ->orWhere('Sct.Description', 'like', "%{$searchValue}%")
+                    ->orWhere('LPr.Description', 'like', "%{$searchValue}%");
+                });
+            }
+        return $query;
     }
 }
